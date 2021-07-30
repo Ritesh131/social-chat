60fe278436b898555bd86f23a99d9f425cf928f1
Ritesh Bajpai <ritesh@spliceglobal.com>
2021-07-30 12:32:07 +0530

    code

--- users/settings.py
+++ users/settings.py
@@ -133,12 +133,12 @@ CHANNEL_LAYERS = {
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
